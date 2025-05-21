911107fab355826311d6030b54fae3bd86f6c80f
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2025-05-21 16:45:43 -0500

    Revert "Fix files"
    This reverts commit 2b965e8e8fb8838602103f2eeb630a45b53f674f.

--- build-and-run.bat
+++ build-and-run.bat
@@ -4,34 +4,16 @@ echo Building CaseyOS using Docker...
 REM Build the Docker image
 docker-compose build
 
-REM First run just the compilation steps (not the image creation)
-docker-compose run --rm caseyos-dev bash -c "make clean && nasm -f bin -o boot.bin boot/boot.asm && make kernel.bin"
+REM Run the build process in the Docker container
+docker-compose run --rm caseyos-dev make clean all
 
-REM Check if the compilation was successful
+REM Check if the build was successful
 if %ERRORLEVEL% neq 0 (
-    echo Compilation failed! Check the errors above.
-    exit /b %ERRORLEVEL%
-)
-
-REM Create the disk image on the host system
-echo Creating OS disk image locally...
-type NUL > os_image.img
-fsutil file seteof os_image.img 1474560
-
-REM Run the Docker container again to finish building the image
-docker-compose run --rm caseyos-dev bash -c "dd if=boot.bin of=os_image.img conv=notrunc && dd if=kernel.bin of=os_image.img seek=1 conv=notrunc"
-
-REM Check if the image creation was successful
-if %ERRORLEVEL% neq 0 (
-    echo Image creation failed! Check the errors above.
+    echo Build failed! Check the errors above.
     exit /b %ERRORLEVEL%
 )
 
 echo Build successful! Running QEMU...
 
-REM Wait a moment to ensure any file handles are properly closed
-timeout /t 2 /nobreak > nul
-
-REM Run QEMU with debugging enabled in Docker with curses display 
-REM Use read-only flag since we don't need to write to the disk image during emulation
-docker-compose run --rm caseyos-dev qemu-system-i386 -display curses -drive file=os_image.img,format=raw,index=0,if=floppy,readonly=on -no-reboot
+REM Run QEMU with debugging enabled
+qemu-system-i386 -drive file=os_image.img,format=raw,index=0,if=floppy -no-reboot
--- build-and-run.sh
+++ build-and-run.sh
@@ -6,23 +6,16 @@ echo "Building CaseyOS using Docker..."
 docker-compose build
 
 # Run the build process in the Docker container
-docker-compose run --rm caseyos-dev bash -c "make clean && make all"
-
-# Check if the build was successful
-if [ $? -ne 0 ]; then
-    echo "Build failed! Check the errors above."
-    exit 1
-fi
-
-echo "Build successful! Running QEMU..."
+docker-compose run --rm caseyos-dev make clean all
 
 # Run QEMU to test the OS
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # For macOS, we need to handle display differently
-  docker-compose run --rm caseyos-dev qemu-system-i386 -fda os_image.img -display cocoa
+  docker run -it --rm -v $(pwd):/caseyos caseyos-caseyos-dev qemu-system-i386 -fda /caseyos/os_image.img -display cocoa
 else
   # For Linux
-  docker-compose run --rm caseyos-dev qemu-system-i386 -display curses -fda os_image.img
+  docker run -it --rm -v $(pwd):/caseyos -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix caseyos-caseyos-dev qemu-system-i386 -fda /caseyos/os_image.img
 fi
 
+echo "Build process complete!"
 echo "If you want to exit QEMU, press Ctrl+Alt+2, then type 'quit' and press Enter" 
