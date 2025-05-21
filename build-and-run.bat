9d3c2648e20b83dc44b4820a7855ed18177247bf
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2025-05-21 12:42:00 -0500

    Run build on host

--- build-and-run.bat
+++ build-and-run.bat
@@ -7,8 +7,15 @@ docker-compose build
 REM Run the build process in the Docker container
 docker-compose run --rm caseyos-dev make clean all
 
-REM Run QEMU with curses display (text-based UI) and debug output
-docker run -it --rm -v %cd%:/caseyos caseyos-caseyos-dev qemu-system-i386 -fda /caseyos/os_image.img -display curses -serial stdio
+REM Check if the build was successful
+if %ERRORLEVEL% neq 0 (
+    echo Build failed! Check the errors above.
+    exit /b %ERRORLEVEL%
+)
 
-echo Build process complete!
+echo Build successful! Running QEMU...
+
+REM Run QEMU with debugging enabled
+qemu-system-i386 -fda os_image.img -monitor stdio -no-reboot
+
 echo If you want to exit QEMU, press Ctrl+Alt+2, then type 'quit' and press Enter 
